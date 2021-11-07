406d8c9b1170661a3b961da81e1a4db51f96c620
Tosin <oladejit3@gmail.com>
2021-11-07 02:27:29 -0800

    added changes

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(express.static("public"));
-
-app.listen(3000, () => {
-  console.log(`server is running on port ${3000}`);
-});
 
 // view engine
 app.set("view engine", "ejs");
@@ -16,6 +13,29 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+// database connection
+const connectDB = async () => {
+  try {
+    // mongodb connection string
+    const con = await mongoose.connect(
+      "mongodb+srv://user2:simple03@node-tutorial.rsb65.mongodb.net/newUser?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log(`mongoDB connected: ${con.connection.host}`);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+connectDB();
+
 app.get("/smoothies", (req, res) => {
   res.render("smoothies");
+});
+
+app.listen(3000, () => {
+  console.log(`server is running on port ${3000}`);
 });

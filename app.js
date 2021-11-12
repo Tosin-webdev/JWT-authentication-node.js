62c74a76c5a6729cd2cbc06e28f378c0e4fd1f3d
Tosin <oladejit3@gmail.com>
2021-11-12 06:13:27 +0100

    added changes

--- app.js
+++ app.js
@@ -38,6 +38,22 @@ connectDB();
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 
+app.get("/set-cookies", (req, res) => {
+  // res.setHeader('Set-Cookie', 'newUser=true')
+  res.cookie("newUser", false);
+  res.cookie("isEmployee", true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  });
+  res.send("you got the cookies");
+});
+
+app.get("/read-cookies", (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+  res.json(cookies);
+});
+
 app.listen(3000, () => {
   console.log(`server is running on port ${PORT}`);
 });

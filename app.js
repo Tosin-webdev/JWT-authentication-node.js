98bd74d0da9cafba2657545ede8fabb743f85e09
Tosin <oladejit3@gmail.com>
2021-11-16 10:49:58 +0100

    added changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
-
+app.use(cookieParser());
 // env file
 dotenv.config({ path: ".env" });
 
@@ -38,21 +39,21 @@ connectDB();
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 
-app.get("/set-cookies", (req, res) => {
-  // res.setHeader('Set-Cookie', 'newUser=true')
-  res.cookie("newUser", false);
-  res.cookie("isEmployee", true, {
-    maxAge: 1000 * 60 * 60 * 24,
-    httpOnly: true,
-  });
-  res.send("you got the cookies");
-});
+// app.get("/set-cookies", (req, res) => {
+//   // res.setHeader('Set-Cookie', 'newUser=true')
+//   res.cookie("newUser", false);
+//   res.cookie("isEmployee", true, {
+//     maxAge: 1000 * 60 * 60 * 24,
+//     httpOnly: true,
+//   });
+//   res.send("you got the cookies");
+// });
 
-app.get("/read-cookies", (req, res) => {
-  const cookies = req.cookies;
-  console.log(cookies.newUser);
-  res.json(cookies);
-});
+// app.get("/read-cookies", (req, res) => {
+//   const cookies = req.cookies;
+//   console.log(cookies.newUser);
+//   res.json(cookies);
+// });
 
 app.listen(3000, () => {
   console.log(`server is running on port ${PORT}`);
--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -28,7 +28,7 @@ const handleErrors = (err) => {
 const maxAge = 3 * 24 * 60 * 60;
 
 const createToken = (id) => {
-  return jwr.sign({ id }, "king frosh secret", {
+  return jwt.sign({ id }, "king frosh secret", {
     expiresIn: maxAge,
   });
 };
@@ -49,7 +49,7 @@ module.exports.signup_post = async (req, res) => {
   try {
     const user = await User.create({ email, password });
     const token = createToken(user._id);
-    res.cookie("jwt", token), { httpOnly: true, maxAge: maxAge * 1000 };
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(201).json({ user: user._id });
   } catch (err) {
     const errors = handleErrors(err);
